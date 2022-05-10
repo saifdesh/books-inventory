cf8cc3be1af3c8345b1117ba9e9625a8d03bacdb
saifdesh <saifurs.87@gmail.comm>
2022-05-10 18:42:18 +0600

    update signing form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
+        <div className='form-container'>
+
+            <div>
+                <div >
+                    <h4 className='form-title'>Login please ..</h4>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            {/* <Form.Label>Email address</Form.Label> */}
+                            <Form.Control className='input-group' type="email" placeholder="Enter email" required />
+
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            {/* <Form.Label>Password</Form.Label> */}
+                            <Form.Control className='input-group' type="password" placeholder="Password" required />
+                        </Form.Group>
+
+                        <Button className='form-submit' variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                    For New user <Link className='form-link' to='/registration'>Plz create an account</Link>
+
+
+
+
+                </div>
+                {/* <h1> OR</h1>
+
+                <div>
+                    <Button className='form-submit' onClick={handleGoogleSignIn}>
+                        <img src={img} /> Google sign in</Button>
+                </div> */}
+
+            </div>
+
+
+
 
         </div>
     );
--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Registration.css';
 
 const Registration = () => {
     return (
-        <div>
-            <h1>Registration</h1>
+        <div className='form-container'>
+            <div >
+                <h4 className='form-title'>Registration please ..</h4>
+                <Form >
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        {/* <Form.Label>Email address</Form.Label> */}
+                        <Form.Control className='input-group' type="email" placeholder="Enter email" required />
+
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        {/* <Form.Label>Password</Form.Label> */}
+                        <Form.Control className='input-group' type="password" placeholder="Password" required />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        {/* <Form.Label>Confirm Password</Form.Label> */}
+                        <Form.Control className='input-group' type="password" placeholder="Confirm Password" required />
+                    </Form.Group>
+
+
+                    <Button className='form-submit' variant="primary" type="Signup">
+                        Registration
+                    </Button>
+                </Form>
+                Already Have an account?  <Link className='form-link' to='/login'>Plz Login</Link>
+
+
+
+
+            </div>
+
+
 
         </div>
     );
